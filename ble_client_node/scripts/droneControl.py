61c48044cb47f7102dff687b582da01bb6b00c6f
AlexanderTemper <41768600+AlexanderTemper@users.noreply.github.com>
2018-11-22 17:04:27 +0100

    Update droneControl.py
    Add LightTelemetry A and S Frame

--- ble_client_node/scripts/droneControl.py
+++ ble_client_node/scripts/droneControl.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 
 import pygatt
-import time
 import rospy
 from sensor_msgs.msg import Joy
-from functools import partial
 
 
 # Many devices, e.g. Fitbit, use random addressing - this is required to
@@ -58,7 +56,8 @@ def joy_callback(data):
     watchdog = data.header.seq;
     yaw = int((((-1*data.axes[0])+1))*1023.5)
 
-    thrust = 0 if (data.axes[1] < 0.0) else int((data.axes[1])*2047)
+    t = ((1-data.axes[5])/2)
+    thrust = 0 if (t == 0) else int(t*512+512)
     roll  = int((((-1*data.axes[3])+1))*1023.5)  
     pitch = int((data.axes[4]+1)*1023.5);
     arm   = data.buttons[4]*2047
@@ -84,6 +83,41 @@ def sendControlData(device):
     except pygatt.exceptions.NotConnectedError:
         return False;
 
+telPitch =0
+telRoll =0
+telYaw =0
+telBat =0
+def handle_data(handle, value):
+    global telPitch,telRoll,telYaw,telBat
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+
+    if value[0]==0x24 and value[1]==0x54: #start $T
+        if value[2]==0x41 and len(value)==10:# A
+            telPitch = twos_comp(value[3]+(value[4]<<8))
+            telRoll  = twos_comp(value[5]+(value[6]<<8))
+            telYaw   = twos_comp(value[7]+(value[8]<<8))
+            #rospy.loginfo("A FRAME: [%d;%d;%d] crc:%d]", telPitch, telRoll, telYaw,value[9])
+        if value[2]==0x53 and len(value)==11:# S
+            telBat = twos_comp(value[3]+(value[4]<<8))
+            #telCurr  = twos_comp(value[5]+(value[6]<<8))
+            #telRSSI   = value[7]
+            #telAirSpeed   = value[8]
+            #telFlightMode   = value[9]
+            #rospy.loginfo("S FRAME: BAT:%d MODE:%s crc:%d", telBat, value[9], value[10] )
+        #else:
+           #rospy.loginfo("%d %s",len(value), binascii.hexlify(value))
+    rospy.loginfo("%.3f [%3d %3d %3d]",(float(telBat)/1000), telPitch, telRoll, telYaw)          
+           
+ 
+def twos_comp(val):
+    """compute the 2's complement of int value val"""
+    if (val & 0x8000): # if sign bit is set e.g., 8bit: 128-255
+        val = val - (0x10000)# compute negative value
+    return val# return positive value as is
+
 
 def main(): 
     global watchdog_old
@@ -102,6 +136,8 @@ def main():
                 device = connect()
                 connected = True if device is not None else False
 
+            if connected:
+                device.subscribe("2d30c082-f39f-4ce6-923f-3484ea480596",callback=handle_data)
             if watchdog_old < watchdog: # only send Data if we get new Data From Joy
                 watchdog_old = watchdog
                 if connected:
